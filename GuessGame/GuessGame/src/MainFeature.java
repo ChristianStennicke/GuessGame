c2d3e196ad7a200ffb4cd00d95d822b4c6eb0b1e
ChristianStennicke <113089191+ChristianStennicke@users.noreply.github.com>
2022-11-01 11:07:13 +0100

    MainFeature + toString method

---
+++ GuessGame/GuessGame/src/MainFeature.java
@@ -0,0 +1,9 @@
+public class MainFeature {
+    @Override
+    public String toString() {
+        String str = "this is a main feature";
+        System.out.println(str);
+        return str;
+
+    }
+}
