64bae9b3e191579d8e439007f6a2c7a6a77b9727
ChristianStennicke <113089191+ChristianStennicke@users.noreply.github.com>
2022-11-01 11:25:59 +0100

    Test 1

---
+++ GuessGame/GuessGame/src/Main.java
@@ -0,0 +1,11 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+     NewFeature feature = new NewFeature();
+     MainFeature feature1 = new MainFeature();
+
+        System.out.println(feature);
+        System.out.println(feature1);
+    }
+}
